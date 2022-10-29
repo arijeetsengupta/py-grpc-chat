2fb1f186a6ea0a637ebac8eb6e8fa5e69024c57d
Ipshita Mahapatra <49113902+ipshitamahapatra@users.noreply.github.com>
2022-10-29 12:14:06 +0530

    Destroying window once user selects one of the options

--- src/client/frame/group_chat_entry.py
+++ src/client/frame/group_chat_entry.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from turtle import width
 from src.client.frame.base import BaseChatFrame
 from one_one_window import one_one_chat
 from tkinter.messagebox import showinfo
@@ -12,19 +13,31 @@ from src.client.frame.group_chat import display_groups
 def group_chat_module(username):
 
     parent = tk.Tk()
+    parent.geometry('300x150')
     frame = tk.Frame(parent)
     frame.pack()
 
+    def create_group_module():
+        parent.destroy()
+        create_group(username)
+    
+    def display_group_module():
+        parent.destroy()  
+        display_groups(username)
+
     text_disp = tk.Button(frame,
                           text="Create New Group",
-                          command=lambda: create_group(username)
+                          command=lambda: create_group_module()
                           )
 
-    text_disp.pack(side=tk.LEFT)
+    text_disp.pack(side=tk.LEFT, pady= 5, padx= 10)
 
     exit_button = tk.Button(frame,
                             text="Join Group",
-                            command=lambda: display_groups(username))
-    exit_button.pack(side=tk.RIGHT)
+                            command=lambda: display_group_module())
+    exit_button.pack(side=tk.RIGHT, pady= 5, padx= 10)
 
     parent.mainloop()
+
+
+
